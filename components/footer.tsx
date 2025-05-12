e44ac9731611c2722587e8d0e936d27df4b34773
EshaqQasem <eshakqasim3@gmail.com>
2025-05-12 23:42:26 +0300

    fix bug on home

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+// import { Input } from "@/components/ui/input"
 import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react"
 import { type Settings } from "@/lib/api"
 import WhatsAppButton from "./whatsapp-button"
