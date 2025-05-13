a6ff156960fff7a00f4a38a8bda3296826bdc013
EshaqQasem <eshakqasim3@gmail.com>
2025-05-13 18:22:07 +0300

    add settings context

--- app/client.tsx
+++ app/client.tsx
@@ -91,7 +91,7 @@ import { SettingsProvider, useSettings } from "@/context/SettingsContext"
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <SettingsProvider>
-      <LayoutContent children={children} />
+      <LayoutContent>{children}</LayoutContent>
     </SettingsProvider>
   )
 }
